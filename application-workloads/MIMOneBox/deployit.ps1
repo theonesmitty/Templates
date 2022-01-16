14adc7a57867755f4ba48f3cce6800b68f467a10
Paul Smith <Paul.Smith@microsoft.com>
2022-01-16 12:03:02 -0800

    updated

--- application-workloads/MIMOneBox/deployit.ps1
+++ application-workloads/MIMOneBox/deployit.ps1
@@ -3,8 +3,8 @@ $rnd = (Get-Random -Minimum 100 -Maximum 999)
 $resourceGroupName = "bicepRG1" + $rnd
 $domainPrefix = "smittynet1" + $rnd
 $domainName = $domainPrefix + ".net"
-$templatePath = "https://raw.githubusercontent.com/theonesmitty/Templates/master/application-workloads/active-directory/active-directory-new-domain/azuredeploy.json"
-
+$templatePath = "https://raw.githubusercontent.com/theonesmitty/Templates/master/application-workloads/MIMOneBox/azuredeploy.json"
+$administratorLogin = "paulsmith"
 $administratorLoginPassword = ConvertTo-SecureString 'Kapper99!!22' -AsPlainText -Force
 
 $params = @{}
